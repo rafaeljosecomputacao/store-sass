3671489c884ef4dc4f48589ee5da66ef234c9d3f
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2025-11-18 20:04:37 -0300

    create-header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// Responsive menu
+function NavbarToggle() {
+    let navbarToggle = document.querySelector('.header-bottom-navbar');
+
+    if (navbarToggle.style.display === "none") {
+        navbarToggle.style.display = "block";
+    } else {
+        navbarToggle.style.display = "none";
+    }
+}
+
 // Current year for footer
 var currentYear = new Date();
 document.getElementById('currentYear').innerHTML = currentYear.getFullYear();
